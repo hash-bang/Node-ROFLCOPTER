85a6a14265cb60433bdfcca041c9297fe9bee1e9
Matt Carter <m@ttcarter.com>
2014-09-15 17:03:30 +1000

    Huge changes to design to make CSS actually work this time

--- app/jquery/vbox.js
+++ app/jquery/vbox.js
@@ -2,7 +2,11 @@ $(function() {
 	// Force all .row-vbox'es to set to window height
 	$(window)
 		.on('resize', function() {
-			$('.row-vbox').css('height', $(window).height());
+			$('.row-vbox')
+				.css('height', $(window).height() - 20)
+				.find('.vbox-inherit-parent').each(function() {
+					$(this).css('height', $(this).closest('.row-vbox > div').height() - 45);
+				});
 		})
 		.trigger('resize');
 	setTimeout(function() {
