fba78c8fd1ef06dacab72f25f14cb8809b853d03
Matt Carter <m@ttcarter.com>
2014-09-15 17:22:25 +1000

    Added auto log scrolling

--- app/controllers/log.js
+++ app/controllers/log.js
@@ -1,10 +1,13 @@
 app.controller('logController', function($scope, $rootScope, $timeout, Logs) {
 	$scope.logs = [];
+	$scope.autoScroll = true;
 
 	$scope.refresh = function() {
 		Logs.query().$promise
 			.then(function(data) {
 				$scope.logs = data;
+				if ($scope.autoScroll)
+					$('#logArea').scrollTo('100%');
 			})
 			.finally(function() {
 				if ($scope.config.autoRefresh.logs)
@@ -12,4 +15,18 @@ app.controller('logController', function($scope, $rootScope, $timeout, Logs) {
 			});
 	};
 	$scope.refresh();
+
+	$scope.setAutoScroll = function(method) {
+		if (method == 'toggle') {
+			$scope.autoScroll = ! $scope.autoScroll;
+		} else {
+			$scope.autoScroll = method;
+		}
+	};
+});
+
+$(function() {
+	$(document).on('mousewheel', '#logArea', function() { // When mouse scrolling - set the autoScroll to off
+		angular.element($('#logController')).scope().setAutoScroll(false);
+	});
 });
