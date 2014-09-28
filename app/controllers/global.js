5536ce89a4317c8026975130d86962306c04ec1e
Matt Carter <m@ttcarter.com>
2014-09-28 15:31:34 +1000

    Seperated data feeds out so info panels now work independently

--- app/controllers/global.js
+++ app/controllers/global.js
@@ -1,17 +1,3 @@
 // App global controller (also $rootScope)
-app.controller('globalController', function($scope, $rootScope, $timeout, Device) {
-	$rootScope.droid = {};
-	$scope.id = 'nova';
-
-	$scope.refresh = function() {
-		Device.get({id: $scope.id}).$promise
-			.then(function(data) {
-				$rootScope.droid = data;
-			})
-			.finally(function() {
-				if ($scope.config.autoRefresh.device)
-					$timeout($scope.refresh, $scope.config.autoRefresh.device);
-			});
-	};
-	$scope.refresh();
+app.controller('globalController', function($scope, $rootScope) {
 });
--- app/controllers/info.js
+++ app/controllers/info.js
@@ -1,9 +1,25 @@
-app.controller('infoController', function($scope) {
+app.controller('infoController', function($scope, $timeout, Device) {
+	var device = {};
+
 	if (!$scope.id)
 		$scope.id = $scope.config.devices.drone;
 
-	$scope.$watch('droid', function() {
-		if ($scope.droid && $scope.droid.rotation && $scope.droid.rotation.x)
-			$('#cube').css('transform', 'rotateX(' + $scope.droid.rotation.x + 'deg) ' + 'rotateY(' + $scope.droid.rotation.y + 'deg) ' + 'rotateZ(' + $scope.droid.rotation.z + 'deg)');
+	// Refresh {{{
+	$scope.refresh = function() {
+		Device.get({id: $scope.id}).$promise
+			.then(function(data) {
+				$scope.device = data;
+			})
+			.finally(function() {
+				if ($scope.config.autoRefresh.device)
+					$timeout($scope.refresh, $scope.config.autoRefresh.device);
+			});
+	};
+	$scope.refresh();
+	// }}}
+
+	$scope.$watch('device', function() {
+		if ($scope.device && $scope.device.rotation && $scope.device.rotation.x)
+			$('#cube').css('transform', 'rotateX(' + $scope.device.rotation.x + 'deg) ' + 'rotateY(' + $scope.device.rotation.y + 'deg) ' + 'rotateZ(' + $scope.device.rotation.z + 'deg)');
 	});
 });
